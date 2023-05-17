974f9bbdad6d5fa46fbd63e989070a29e55d2f2f
shawnn <shawn80324@gmail.com>
2023-05-17 14:09:51 +0800

    fix ml error acc

--- coffee_beans_ml_classification/ml_classificatioin.py
+++ coffee_beans_ml_classification/ml_classificatioin.py
@@ -31,9 +31,9 @@ if argparse_module:
     parser.add_argument('--training_data_path',type=str,default='./training_process_data/',help='output training data path')
     parser.add_argument('--image_size',type=int,default= 64,help='image size')
     parser.add_argument('--num_classes',type=int,default= 2,help='num classes')
-    parser.add_argument('--model',type=str,default='xgboost')
+    parser.add_argument('--model',type=str,default='svm')
     parser.add_argument('--lr',type= int,default= 0.3,help='learningrate')
-    parser.add_argument('--n_estimators',type=int,default=200)
+    parser.add_argument('--n_estimators',type=int,default=100)
     args = parser.parse_args()
 """----------argparse init end----------"""
 """----------function----------"""
@@ -55,19 +55,50 @@ def data_process(classes_dir):
             image = Image.open(path)
             image = image.resize((args.image_size, args.image_size))
             image = np.array(image)
-            image_array[i,:,:]=image
+            image_array[j,:,:]=image
         if i ==0 :
             x = image_array
         else:
             x =np.concatenate((x,image_array),axis= 0)
     X_train,X_test,y_train,y_test =train_test_split(x,y,test_size=0.2,random_state= 10,shuffle=True)
     return X_train,X_test,y_train,y_test 
+def data_process_means(classes_dir):
+    x = np.zeros(0)
+    y = np.zeros(0,dtype=np.uint8)
+    for i, classes in enumerate(classes_dir):
+        class_dir = os.path.join(args.database_path,classes)
+        file_path = os.listdir(class_dir)
+        length = len(file_path)
+        # process image
+        image_array = np.zeros((length,3),dtype=np.float16)
+        # process label    
+        y_len = np.full(length,i,dtype=np.uint8)
+        y = np.concatenate((y,y_len),axis= 0)
+
+        for j,file in enumerate(file_path):
+            path = os.path.join(class_dir,file)
+            image = Image.open(path)
+            image = image.resize((args.image_size, args.image_size))
+            image = np.array(image)
+            image_mean_r = np.mean(image[0])
+            image_mean_g = np.mean(image[1])
+            image_mean_b = np.mean(image[2])
+            image_mean = np.array((image_mean_r,image_mean_g,image_mean_b))
+            image_array[j,:]=image_mean
+            #print(image_array)
+        if i ==0 :
+            x = image_array
+        else:
+            x =np.concatenate((x,image_array),axis= 0)
+    X_train,X_test,y_train,y_test =train_test_split(x,y,test_size=0.2,random_state= 10,shuffle=True)
+    return X_train,X_test,y_train,y_test
 
 """----------function end----------"""
 
 """----------main----------"""
 if __name__ == '__main__':
-    X_train,X_test,y_train,y_test = data_process(os.listdir(args.database_path))
+    #X_train,X_test,y_train,y_test = data_process(os.listdir(args.database_path))
+    X_train,X_test,y_train,y_test = data_process_means(os.listdir(args.database_path))
     #影像降維
     X_train=X_train.reshape(len(X_train),-1)
     X_test = X_test.reshape(len(X_test),-1)
